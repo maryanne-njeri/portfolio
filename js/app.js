693349b1fb9921cfedaf7691df126f664301423c
Rosemary Gathua <maryanjeryrose@gmail.com>
2020-05-15 23:15:09 +0300

    adds menu toggle button

---
+++ js/app.js
@@ -0,0 +1,9 @@
+function toggleMenu() {
+	let toggle = document.getElementById('navBar');
+
+	if (toggle.style.display === "none") {
+		toggle.style.display = "block";
+	} else {
+		toggle.style.display = "none";
+	}
+}
